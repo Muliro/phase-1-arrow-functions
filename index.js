b65712577c9beedfce16223f7ac8de9237d29c93
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-09 19:49:32 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function(){ // simple function expression
+    return 2000 / 100;
+}
+
+const square = x => x**2;  // simple arrow function
+
+
+const add = (a, b) => a + b; // addition arrow functtion
